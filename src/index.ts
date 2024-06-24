56ce28132095a4b2798b993984f4d88815f487f6
Mr.PAPI <thesnakewitcher@gmail.com>
2024-06-24 09:49:17 -0300

    feat: add top index.ts module

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// https://www.npmjs.com/package/hardhat-local-networks-config-plugin
+export { NetworkConfig } from "./network-config"
+export { NetworkConfigs, createNetworkConfigs } from "./network-configs"
